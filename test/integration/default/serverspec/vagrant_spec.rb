99ed4363f12fe3a7250554b15698edbd363cb099
juju4 <juju4@users.noreply.github.com>
2017-01-05 02:11:26 -0500

    remove known fail test, apt module ansible and travis context

--- test/integration/default/serverspec/vagrant_spec.rb
+++ test/integration/default/serverspec/vagrant_spec.rb
@@ -7,11 +7,14 @@ describe file('/usr/bin/vagrant') do
   it { should be_executable }
 end
 
-describe command('vagrant plugin list') do
-  its(:stdout) { should match /scp/ }
-  its(:stdout) { should match /winrm/ }
-  its(:stdout) { should match /digitalocean/ }
-  its(:stdout) { should match /aws/ }
-  its(:stdout) { should match /vsphere/ }
-end
+if ENV['SERVERSPEC_WITH_VAGRANT'] == '1'
 
+  describe command('vagrant plugin list') do
+    its(:stdout) { should match /scp/ }
+    its(:stdout) { should match /winrm/ }
+    its(:stdout) { should match /digitalocean/ }
+    its(:stdout) { should match /aws/ }
+    its(:stdout) { should match /vsphere/ }
+  end
+
+end
