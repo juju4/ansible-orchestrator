87fa97592aa22c40fa291e3bebc14df00275a95a
juju4 <juju4@users.noreply.github.com>
2021-02-27 10:31:53 -0500

    test/default: update ansible path validation as pip install except centos7

--- test/integration/default/serverspec/ansible_spec.rb
+++ test/integration/default/serverspec/ansible_spec.rb
@@ -3,7 +3,12 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/usr/bin/ansible') do
+set ansible_path = '/usr/local/bin/ansible'
+if (os[:family] == 'redhat' && os[:release] == '7')
+  set ansible_path = '/usr/bin/ansible'
+end
+
+describe file(ansible_path) do
   it { should be_executable }
 end
 
